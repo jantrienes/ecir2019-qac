5456dd0f66b2be9cdb1f1b0b170a57fe677f47ff
Jan Trienes <jan.trienes@googlemail.com>
2018-12-05 19:26:19 +0100

    Replace hardcoded community with variable

--- scripts/generate_data.sh
+++ scripts/generate_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-community="unix"
+community="datascience"
 xml_dir=data/"$community".stackexchange.com/
 out_dir=data
 
@@ -7,6 +7,6 @@ python qac/dataset/load_data.py "$community" "$xml_dir"
 python qac/dataset/annotate_data.py "$community"
 python qac/dataset/create_indexes.py
 python qac/dataset/dump_community.py "$community" "$out_dir"/labeled/
-python qac/dataset/dump_clarq.py unix "$out_dir"/clarq
+python qac/dataset/dump_clarq.py "$community" "$out_dir"/clarq
 
 python qac/dataset/data_analysis.py "$community" "$out_dir/labeled/$community.csv"
